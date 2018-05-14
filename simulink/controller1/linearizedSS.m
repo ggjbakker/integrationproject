cb1be89e3b8fb5bba1e33d61b97d2378173296e9
Geert Bakker <geert.bakker@oxolutions.nl>
2018-05-14 14:57:08 +0200

    Tuned LQR controller without observer

--- simulink/controller1/linearizedSS.m
+++ simulink/controller1/linearizedSS.m
@@ -3,7 +3,7 @@
 %Parameters
 K = 1.8; %motor gain
 k1 = 0.35; %friction coefficient first link
-k2 = 0.0002; %friction coefficient second link
+k2 = 0.00001; %friction coefficient second link
 g = 9.81; %Gravity
 l1 = 0.1; %length of first link in m
 l2 = 0.1; %length of second link in m
@@ -21,7 +21,7 @@ gamma = (m1*c1^2+m2*l1^2+I1);
 v = (m2*c2^2+I2);
 A = [0 0 1 0;
      0 0 0 1;
-     g*v*beta/alpha -c2^2*g*l1*m2^2/alpha -k1*v/a c2*k2*l1*m2/a; 
+     g*v*beta/alpha -c2^2*g*l1*m2^2/alpha -k1*v/alpha c2*k2*l1*m2/alpha; 
      -c2*g*l1*m2*beta/alpha c2*g*m2*gamma/alpha c2*k1*l1*m2/alpha -k2*gamma/alpha];
 B = [0;0;K*v/alpha;-c2*l1*m2/alpha];
 C = [1 0 0 0;0 1 0 0];
@@ -35,7 +35,7 @@ gamma = (m1*c1^2+m2*l1^2+I1);
 v = (m2*c2^2+I2);
 A = [0 0 1 0;
      0 0 0 1;
-     -g*v*beta/alpha c2^2*g*l1*m2^2/alpha -k1*v/a c2*k2*l1*m2/a; 
+     -g*v*beta/alpha c2^2*g*l1*m2^2/alpha -k1*v/alpha c2*k2*l1*m2/alpha; 
      c2*g*l1*m2*beta/alpha -c2*g*m2*gamma/alpha c2*k1*l1*m2/alpha -k2*gamma/alpha];
 B = [0;0;K*v/alpha;-c2*l1*m2/alpha];
 C = [1 0 0 0;0 1 0 0];
--- simulink/controller2/controllerData3.m
+++ simulink/controller2/controllerData3.m
@@ -1,14 +1,17 @@
 %Variables
-h = 0.001;
-x0 = [-0.6*pi 0 -0.4*pi 0]; %initial condition
+h = 0.01;
+x0 = [0.55*pi 0.45*pi 0 0]; %initial condition
 sys = linearizedSSuu;
 
 %Controllerparameters
-Q = [10 0 0 0;0 1 0 0;0 0 0 0;
+Q = [10 0 0 0;
+     0 0.01 0 0;
+     0 0 0.001 0;
+     0 0 0 1];
+R = 10^12;
 %Controller
-copoles = [-15 -15.1 -15.2 -15.3];
 
 [A,B,C,D] = ssdata(sys);
-[K,S,e] = lqr(sys,
-
-
+[K,S,e] = lqr(sys,Q,R);
+Ki = [-2 -0.1 0 0];
+K = K+Ki;
